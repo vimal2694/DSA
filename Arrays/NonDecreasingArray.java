38a7109a5d32fcfd074a3f38bcfeca47a0536525
vrawat1 <vimal.rawat@franklintempleton.com>
2023-09-07 14:11:37 +0530

    commit

--- Arrays/NonDecreasingArray.java
+++ Arrays/NonDecreasingArray.java
@@ -1,6 +1,7 @@
 package Arrays;
 
-//Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+//Given an array nums with n integers, your task is to check if it could become non-decreasing 
+//by modifying at most one element.
 
 //We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
 
@@ -8,32 +9,32 @@ public class NonDecreasingArray {
 
 	public static boolean nonDecreasingArray(int arr[]) {
 		boolean isNonDecreasing = true;
-		
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i]<arr[i-1]) {
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < arr[i - 1]) {
 				isNonDecreasing = false;
 				break;
 			}
 		}
 		return isNonDecreasing;
 	}
-	
+
 	public static void main(String args[]) {
-		int arr[] = {-2,7,-1,0,1,2};
-		int count =0;
+		int arr[] = { -2, 7, -1, 0, 1, 2 };
+		int count = 0;
 		boolean flag = true;
-		if(nonDecreasingArray(arr)==false) {
-			
-			for(int i=1;i<arr.length;i++) {
-				if(arr[i]<arr[i-1]) {
+		if (nonDecreasingArray(arr) == false) {
+
+			for (int i = 1; i < arr.length; i++) {
+				if (arr[i] < arr[i - 1]) {
 					count++;
 				}
 			}
 		}
-		
-		if(count > 1) {
+
+		if (count > 1) {
 			flag = false;
 		}
-		System.out.println(flag + "-- count----"+ count);
+		System.out.println(flag + "-- count----" + count);
 	}
 }
