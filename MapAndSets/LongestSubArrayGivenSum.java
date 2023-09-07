e7bd61983cf75f0d9a9cd736bfab0b52ee68c1d1
vrawat1 <vimal.rawat@franklintempleton.com>
2023-09-07 15:15:38 +0530

    Problem statement update

--- MapAndSets/LongestSubArrayGivenSum.java
+++ MapAndSets/LongestSubArrayGivenSum.java
@@ -2,6 +2,23 @@ package MapAndSets;
 
 import java.util.HashMap;
 
+/*
+ * Given an array arr[] of size n containing integers. 
+ * The problem is to find the length of the longest sub-array having sum equal to the given value k.
+ * 
+ * Examples: 
+
+	Input: arr[] = { 10, 5, 2, 7, 1, 9 }, k = 15
+	Output: 4
+	Explanation: The sub-array is {5, 2, 7, 1}.
+	
+	Input: arr[] = {-5, 8, -14, 2, 4, 12}, k = -5
+	Output: 5
+ * 
+ */
+
+
+
 public class LongestSubArrayGivenSum {
 	
 	public static int longestSubArrayGivenSum(int arr[], int k) {
